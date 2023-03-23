e13c86678ac64a14b5167ae26d0b710a9efb6715
Damian Ysla <damianysla1@outlook.com>
2023-03-23 18:20:54 -0300

    Api setup

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 // Llamada a la API de Rick and Morty
-fetch('https://rickandmortyapi.com/api/character/')
-  .then(response => response.json())
-  .then(data => {
-    // Creación de las tarjetas
-    const cardsContainer = document.getElementById('cards-container');
-    for (let i = 0; i < 6; i++) {
-      const character = data.results[i];
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.innerHTML = `
+async function getCharacters() {
+    const characters = [];
+  
+    let response = await fetch('https://rickandmortyapi.com/api/character');
+    let data = await response.json();
+  
+    characters.push(...data.results);
+  
+    let totalPages = data.info.pages;
+  
+    for (let i = 2; i <= totalPages; i++) {
+      response = await fetch(`https://rickandmortyapi.com/api/character?page=${i}`);
+      data = await response.json();
+      characters.push(...data.results);
+    }
+  
+    return characters;
+  }
+  
+getCharacters()
+.then(characters => {
+
+// Creación de las tarjetas
+const cardsContainer = document.getElementById('cards-container'); 
+const firstIndex = Math.floor(Math.random() * characters.length);
+
+
+for (let i = 0; i < 6; i++) {
+    const character = characters[i];
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    if (character.status == "Alive") {class_status = "card__status--alive"} 
+    else if (character.status == "Dead") {class_status = "card__status--dead"}
+    else {class_status = "card__status--unknown"}
+
+    card.innerHTML = `
         <img class="card__img" src="${character.image}" alt="">
         <div>
             <h2 class="card__name">${character.name}</h2>
-            <h3 class="card__status--unknown">${character.status}</h3>
+            <h3 class="${class_status}">${character.status}</h3>
         </div>
         <hr>
         <h3 class="card__species">${character.species}</h3>
         <h3 class="card__gender">${character.gender}</h3>
         <h3 class="card__origin">${character.origin.name}</h3>
-      `;
-      cardsContainer.appendChild(card);
+    `;
+
+    cardsContainer.appendChild(card);
     }
-  })
-  .catch(error => console.error(error));
+})
+.catch(error => console.error(error));
